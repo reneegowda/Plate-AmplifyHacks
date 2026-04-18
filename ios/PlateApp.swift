cc383f7dd988cbc315a41bbf9cb59616ebf85173
Renee <rsg276@cornell.edu>
2026-04-18 12:13:21 -0400

    pushing xcode proj

---
+++ ios/PlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateApp.swift
+//  Plate
+//
+//  Created by Renee Gowda on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct PlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
